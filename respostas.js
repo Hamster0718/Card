251b51b2791cb08b615a2ae826e671b4a0f3b0b4
Hamster0718 <00001101889883sp@al.educacao.sp.gov.br>
2024-12-02 21:11:57 -0300

    att 001

---
+++ respostas.js
@@ -0,0 +1,305 @@
+criaCartao(
+    'DISNEY',
+    'Qual foi o primeiro filme animado da Disney?',
+    'Bambi'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual animal é Bambi?',
+    'Um cervo'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Cinderela, qual objeto mágico ajuda a protagonista a chegar ao baile?',
+    'Uma carruagem feita de abóbora'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é o vilão em A Bela Adormecida?',
+    'Malévola'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual o nome da protagonista de A Pequena Sereia?',
+    'Ariel'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem dubla o gênio em Aladdin na versão original?',
+    'Robin Williams'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em O Rei Leão, qual é o nome do pai de Simba?',
+    'Mufasa'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual música de O Rei Leão ganhou o Oscar?',
+    'Can You Feel the Love Tonight'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em A Bela e a Fera, qual é o objeto encantado que conta o tempo do feitiço?',
+    'Uma rosa'
+)
+
+criaCartao(
+    'DISNEY',
+    'Como se chama o dragão em Mulan?',
+    'Mushu'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o nome do camaleão de Rapunzel em Enrolados?',
+    'Pascal'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é a irmã mais velha em Frozen?',
+    'Elsa'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em A Princesa e o Sapo, qual é o sonho de Tiana?',
+    'Abrir um restaurante'
+)
+
+criaCartao(
+    'DISNEY',
+    'Que princesa da Disney vive na Polinésia?',
+    'Moana'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o nome do príncipe em Branca de Neve e os Sete Anões?',
+    'Ele é conhecido apenas como “Príncipe”'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual foi o primeiro filme da Pixar?',
+    'Toy Story'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Procurando Nemo, qual é o nome do pai de Nemo?',
+    'Marlin'
+)
+
+criaCartao(
+    'DISNEY',
+    'Que tipo de peixe é Dory?',
+    'Cirurgião-patela'
+)
+
+criaCartao(
+    'DISNEY',
+    'ual o trabalho de Sullivan e Mike em Monstros S.A.?',
+    'Assustadores de crianças para gerar energia'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Os Incríveis, qual é o superpoder de Violeta?',
+    'Invisibilidade e criação de campos de força'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é o vilão de Aladdin?',
+    'Jafar'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual vilã quer roubar filhotes de dálmatas para fazer casacos de pele?',
+    'Cruella de Vil'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual vilão transforma pessoas em brinquedos em Pinóquio?',
+    'O Coachman'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é o vilão em Hércules?',
+    'Hades'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Peter Pan, quem é o arqui-inimigo de Peter?',
+    'Capitão Gancho'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual foi o primeiro filme da Disney baseado em um conto de Hans Christian Andersen?',
+    'A Pequena Sereia'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em que filme da Disney aparece a canção “Hakuna Matata”?',
+    'O Rei Leão'
+)
+
+criaCartao(
+    'DISNEY',
+    'Que filme da Disney tem uma lâmpada mágica?',
+    'Aladdin'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o lema de Zootopia?',
+    'Onde qualquer um pode ser qualquer coisa'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é o inventor em A Bela e a Fera?',
+    'Maurice, o pai de Bela'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Frozen II, qual é o elemento mágico de Elsa?',
+    'Gelo/água'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o nome do protagonista em Encanto?',
+    'Mirabel'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é o vilão em Raya e o Último Dragão?',
+    'Não há um vilão único; o principal antagonista é o conflito humano e o Drunn'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Red: Crescer é uma Fera, Mei se transforma em qual animal?',
+    'Um panda vermelho'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual música de Encanto foi um grande sucesso?',
+    'We Don’t Talk About Bruno'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual animal é Dumbo?',
+    'Um elefante'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem é a mãe de Bambi?',
+    'Uma corça; seu nome nunca é revelado'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Alice no País das Maravilhas, quem vive no País das Maravilhas?',
+    'Alice'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Alice no País das Maravilhas, quem vive no País das Maravilhas?',
+    'Alice'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o melhor amigo de Simba?',
+    'Nala'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Tarzan, como se chama a parceira do protagonista?',
+    'Jane'
+)
+
+criaCartao(
+    'DISNEY',
+    'Quem compôs as músicas de A Pequena Sereia e A Bela e a Fera?',
+    'Alan Menken'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual foi o primeiro filme da Disney a ganhar o Oscar de Melhor Animação?',
+    'Branca de Neve, em prêmio honorário'
+)
+
+criaCartao(
+    'DISNEY',
+    'Que música de Encanto viralizou globalmente?',
+    'We Don’t Talk About Bruno'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Pocahontas, qual é o título da música principal?',
+    'Colors of the Wind'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em O Corcunda de Notre Dame, qual é o nome do protagonista?',
+    'Quasímodo'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o lema do Mickey Mouse Club?',
+    'M-I-C-K-E-Y M-O-U-S-E”'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Robin Hood, qual animal é Robin?',
+    'Uma raposa'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em Atlantis: O Reino Perdido, qual é a profissão de Milo?',
+    'Linguista/Arqueólogo'
+)
+
+criaCartao(
+    'DISNEY',
+    'Qual é o nome do cachorro de Miguel em Viva: A Vida é uma Festa?',
+    'Dante'
+)
+
+criaCartao(
+    'DISNEY',
+    'Em que ano O Rei Leão foi lançado?',
+    '1994'
+)
